a942f0a4890ff21ab03b9b9c2985e4dd29ea72cd
Maryan Mayher <mr.maykher@gmail.com>
2016-04-16 16:15:46 +0300

    AngularJS filters 1st commit

---
+++ Trainings/AngularJS Trainings/IntroToAngular/filters.js
@@ -0,0 +1,29 @@
+(function (angular) {
+    angular.module("appModule", []).controller("filterController", function ($scope) {
+        var employees = [
+          {
+                name: 'Maryan'
+                , dateOfBirth: new Date('03/14/1995')
+                , gender: "Male"
+                , salary: 4400
+            }, {
+                name: 'Ivan'
+                , dateOfBirth: new Date('11/04/1997')
+                , gender: "Male"
+                , salary: 2400
+            },{
+                name: 'Anna'
+                , dateOfBirth: new Date('09/08/1993')
+                , gender: "Female"
+                , salary: 3200
+            },{
+                name: 'Petro'
+                , dateOfBirth: new Date('05/14/1985')
+                , gender: "Male"
+                , salary: 1900
+            }
+        ];
+        $scope.employees = employees;
+        $scope.rowLimit = 2;
+    })
+})(angular);
